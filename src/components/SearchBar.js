a7811c511c5311a9d7b916554702e6902439c854
johnpospisil <audiofreak7@gmail.com>
2020-09-11 12:04:35 -0700

    Style the SearchBar. Manage event handlers.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,32 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = () => {};
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar">
+        <form className="">
+          <div className="form-group">
+            <label>Video Search</label>
+            <input
+              className="form-control"
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+          {/* <button
+            className="btn btn-primary"
+            type="submit"
+          >
+            Submit
+          </button> */}
+        </form>
+      </div>
+    );
   }
 }
 
