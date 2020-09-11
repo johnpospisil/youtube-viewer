b2af8f2eed549cdd266943946780df48630fabf4
johnpospisil <audiofreak7@gmail.com>
2020-09-11 14:42:35 -0700

    Import Axios with npm

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyChM11yWdEpbaX5tkrGOa9Buo3hZrc_2Ps";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
