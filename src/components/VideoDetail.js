cecf4b0df69f0d9666a8b551562bb81903292791
johnpospisil <audiofreak7@gmail.com>
2020-09-14 16:46:34 -0700

    Display video player for selected video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,8 +4,19 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div className="container">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe
+          className="embed-responsive-item"
+          title={video.snippet.title}
+          src={videoSrc}
+          allowFullScreen
+        ></iframe>
+      </div>
       <h4>{video.snippet.title}</h4>
       <p>{video.snippet.description}</p>
     </div>
