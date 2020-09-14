dc64cc41859ec7e83ab89d6616570c318fc80661
johnpospisil <audiofreak7@gmail.com>
2020-09-14 14:18:19 -0700

    Display and style VideoItems

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,33 @@
+import "./VideoItem.css";
 import React from "react";
 
-const VideoItem = (props) => {
-  return <div>Video Item</div>;
+const VideoItem = ({ video }) => {
+  return (
+    <div className="video-item">
+      <div className="card mb-3">
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <img
+              src={video.snippet.thumbnails.medium.url}
+              alt={video.snippet.title}
+              className="video-img card-img"
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{video.snippet.title}</h5>
+              <p className="card-text">{video.snippet.description}</p>
+              <p className="card-text">
+                <small className="text-muted">
+                  Published: {video.snippet.publishedAt}
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoItem;
