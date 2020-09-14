a071e7517e176c8b86565525db4af5ee4d6588c9
johnpospisil <audiofreak7@gmail.com>
2020-09-14 16:24:41 -0700

    Display the selected video's title and description

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,7 +4,12 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   }
-  return <div>{video.snippet.title}</div>;
+  return (
+    <div className="container">
+      <h4>{video.snippet.title}</h4>
+      <p>{video.snippet.description}</p>
+    </div>
+  );
 };
 
 export default VideoDetail;
